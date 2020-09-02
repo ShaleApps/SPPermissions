26aefa94781772a9544cd39ccba80cb3af98b3ea
logan johnson <loganbrunesjohnson@gmail.com>
2020-09-02 14:36:17 -0600

    hide right nav bar item

--- Source/SPPermissions/Interface/List/Controllers/SPPermissionsListController.swift
+++ Source/SPPermissions/Interface/List/Controllers/SPPermissionsListController.swift
@@ -76,11 +76,11 @@ public class SPPermissionsListController: UITableViewController, SPPermissionsCo
     override public func viewDidLoad() {
         super.viewDidLoad()
         
-        if #available(iOS 13.0, *) {
-            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(dismissAnimated))
-        } else {
-            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissAnimated))
-        }
+//        if #available(iOS 13.0, *) {
+//            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(dismissAnimated))
+//        } else {
+//            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissAnimated))
+//        }
 
         navigationItem.title = titleText
         navigationItem.largeTitleDisplayMode = .automatic
